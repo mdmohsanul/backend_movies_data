2267ec5535bbb9674fe7294a6dc9dff7648e611c
Mohsanul786 <mdmohsanul@gmail.com>
2024-10-22 16:44:06 +0530

    modified data model

--- Readme.md
+++ Readme.md
@@ -1,1 +1,21 @@
 ## this repository is hosted on server
+
+## In this codebase we learn how to deploy/host our code on server
+
+-- create vercel.json file
+{
+"version": 2,
+"builds": [{ "src": "index.js", "use": "@vercel/node" }],
+"rewrites": [{ "source": "/(.*)", "destination": "index.js" }]
+}
+in src and destination put the correct file name you want to deloy
+
+-- CORS
+
+const cors = require("cors");
+const corsOptions = {
+origin: "\*",
+credentials: true,
+optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
--- models/movie.model.js
+++ models/movie.model.js
@@ -1,19 +1,40 @@
 const mongoose = require("mongoose");
 
-const MoviesSchema = new mongoose.Schema({
-  title: String,
-  releaseYear: Number,
-  genre: [{ type: String }],
-  director: String,
-  actors: [{ type: String }],
-  language: String,
-  country: String,
-  rating: Number,
-  plot: String,
-  awards: String,
-  posterUrl: String,
-  trailerUrl: String,
-});
+const MoviesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    releaseYear: {
+      type: String,
+    },
+    genre: [{ type: String }],
+    director: {
+      type: String,
+      required: true,
+    },
+    actors: [{ type: String }],
+    language: String,
+    country: {
+      type: String,
+      default: "India",
+    },
+    rating: {
+      type: String,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    plot: String,
+    awards: String,
+    posterUrl: String,
+    trailerUrl: String,
+  },
+  {
+    timestamp: true,
+  }
+);
 
 const Movies = mongoose.model("Movies", MoviesSchema);
 module.exports = Movies;
